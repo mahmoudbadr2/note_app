ccd71b246014133161d18b7c6a993ff772e9d6d0
mahmoudbadr2 <126113674+mahmoudbadr2@users.noreply.github.com>
2024-01-21 02:01:04 +0200

    display notes part 1

--- lib/cubits/notes_cubit/notes_cubit_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit_cubit.dart
@@ -8,16 +8,10 @@ part 'notes_cubit_state.dart';
 
 class NotesCubit extends Cubit<NotesCubitState> {
   NotesCubit() : super(NotesCubitInitial());
-
+  List<NoteModel>? notes;
   fetchAllNotes() async {
-    emit(NotesLooding());
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotes);
+    var notesBox = Hive.box<NoteModel>(kNotes);
 
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+    notes = notesBox.values.toList();
   }
 }
--- lib/cubits/notes_cubit/notes_cubit_state.dart
+++ lib/cubits/notes_cubit/notes_cubit_state.dart
@@ -4,17 +4,3 @@ part of 'notes_cubit_cubit.dart';
 sealed class NotesCubitState {}
 
 final class NotesCubitInitial extends NotesCubitState {}
-
-final class NotesLooding extends NotesCubitState {}
-
-final class NotesSuccess extends NotesCubitState {
-  final List<NoteModel> notes;
-
-  NotesSuccess(this.notes);
-}
-
-final class NotesFailure extends NotesCubitState {
-  final String errMessage;
-
-  NotesFailure(this.errMessage);
-}
